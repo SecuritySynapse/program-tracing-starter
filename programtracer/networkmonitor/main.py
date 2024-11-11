83b01121845737d14594508ca684729a42d9cd7f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-11 08:23:52 -0500

    chore: Add the program tracing file.

---
+++ programtracer/networkmonitor/main.py
@@ -0,0 +1,4 @@
+"""Perform program tracing for the purposes of malware analysis."""
+
+# TODO: Add any necessary source code to perform the program
+# tracing tasks and analyses that are outlined in the README.
